c5c1706dab6acc5ea8f8d081d38b9b325865c627
Zeki T. Luan <s1324151534@outlook.com>
2024-04-19 10:04:10 +0800

    Update 1.2

--- js/index.js
+++ js/index.js
@@ -39,3 +39,44 @@ cities_btn.addEventListener("click", () => {
     }
     bOpenedRight = !bOpenedRight;
 })
+
+let selected_city = "city-0";
+//write code here to load localstorage
+
+const all_city = document.getElementsByClassName("i-right-slide-slist-item");
+
+let i = 0;
+let mouse_pos = 0;
+let offsetX = 0;
+let bClicked = false;
+let clickedID;
+
+for (i = 0; i < all_city.length; ++i) {
+    all_city[i].addEventListener("click", (e) => {
+        if (Math.abs(offsetX) < 20) {
+            let from_city = document.getElementById(selected_city);
+            let to_city = document.getElementById(e.target.id);
+            from_city.setAttribute("class", "i-right-slide-slist-item");
+            to_city.setAttribute("class", "i-right-slide-slist-item i-right-slide-item-selected")
+            selected_city = e.target.id;
+        }
+    })
+    all_city[i].addEventListener("mousedown", (event) => {   
+        mouse_pos = event.screenX;
+        bClicked = true;
+        clickedID = event.target.id;
+    })
+    all_city[i].addEventListener("mousemove", (event) => {
+        offsetX = event.screenX - mouse_pos;
+        if (bClicked) {
+            document.getElementById(event.target.id).style = "transform: translateX(" + offsetX + "px);";
+        }
+    })
+    all_city[i].addEventListener("mouseup", (e) => {
+        bClicked = false;
+        document.getElementById(clickedID).style = "";
+        if (Math.abs(offsetX) > 50) {
+            document.getElementById("slist-box").removeChild(document.getElementById(clickedID));
+        }
+    })
+}
